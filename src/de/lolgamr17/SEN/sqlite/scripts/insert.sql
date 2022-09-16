765c7010a11602168513bd3cbb0accc4b01bd65c
Felix <40723033+DerFelixPlayer2@users.noreply.github.com>
2022-09-16 14:51:30 +0200

    SQLite intro

---
+++ src/de/lolgamr17/SEN/sqlite/scripts/insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO Kuenstler (Name, Gruendungsjahr) 
+VALUES ("Anastacia", 1999);
+INSERT INTO Kuenstler (Name, Gruendungsjahr) 
+VALUES ("Pink Floyd", 1965);
+
+INSERT INTO CD (Albumtitel, IId, Erscheinungsjahr) 
+VALUES ("Not that kind", 5, 2000);
+INSERT INTO CD (Albumtitel, IId, Erscheinungsjahr) 
+VALUES ("Wish you were here", 6, 1975);
+INSERT INTO CD (Albumtitel, IId, Erscheinungsjahr) 
+VALUES ("Free of nature", 5, 2001);
+
+INSERT INTO Song (Track, Titel, CDId) 
+VALUES (1, "Not that kind", 1);
+INSERT INTO Song (Track, Titel, CDId) 
+VALUES (2, "I'm outta love", 1);
+INSERT INTO Song (Track, Titel, CDId) 
+VALUES (3, "Cowboys and Kisses", 1);
+INSERT INTO Song (Track, Titel, CDId) 
+VALUES (1, "Shin on your craxxxy diamond", 2);
+INSERT INTO Song (Track, Titel, CDId) 
+VALUES (1, "Paid my dues", 3);
