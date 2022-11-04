b5ed5427f2e14345a0e160d177d0f4e8b1e61c35
Felix <40723033+DerFelixPlayer2@users.noreply.github.com>
2022-11-04 14:04:26 +0100

    Fix this shit

--- PlatformIO/lib/OLED_SSD1306/oled_ssd1306.cpp
+++ PlatformIO/lib/OLED_SSD1306/oled_ssd1306.cpp
@@ -3,7 +3,7 @@
  *  Author: Ulrich
  */
 
-#include "oled_ssd1306.h"
+#include "oled_ssd1306.hpp"
 
 #include "font.h"
 
@@ -235,4 +235,27 @@ void oled_write_P(const char *Buffer, ...) {
 		}
 	}
 	va_end(ap);
+}
+
+void OLED_SSD1306::main() {
+	DDRB |= (1 << PB5);
+	oled_init();
+	oled_gotoxy(0, 0);
+
+	while (1) {
+		for (uint32_t tmp = 1; tmp < 99999999; tmp++) {
+			oled_gotoxy(0, 0);
+			oled_write("****************");
+			oled_gotoxy(0, 1);
+			oled_write("*     BigH     *");
+			oled_gotoxy(0, 2);
+			oled_write("****************");
+			oled_font_size(1);
+			oled_gotoxy(0, 4);
+			oled_write("%8i", tmp);
+			oled_font_size(0);
+			_delay_ms(100);
+			PORTB ^= (1 << PB5);
+		}
+	}
 }
--- PlatformIO/lib/OLED_SSD1306/oled_ssd1306.hpp
+++ PlatformIO/lib/OLED_SSD1306/oled_ssd1306.hpp
@@ -70,28 +70,7 @@ void oled_write_P(const char* Buffer, ...);
 
 class OLED_SSD1306 {
    public:
-	static void main() {
-		DDRB |= (1 << PB5);
-		oled_init();
-		oled_gotoxy(0, 0);
-
-		while (1) {
-			for (uint32_t tmp = 1; tmp < 99999999; tmp++) {
-				oled_gotoxy(0, 0);
-				oled_write("****************");
-				oled_gotoxy(0, 1);
-				oled_write("*     BigH     *");
-				oled_gotoxy(0, 2);
-				oled_write("****************");
-				oled_font_size(1);
-				oled_gotoxy(0, 4);
-				oled_write("%8i", tmp);
-				oled_font_size(0);
-				_delay_ms(100);
-				PORTB ^= (1 << PB5);
-			}
-		}
-	}
+	static void main();
 };
 
 #endif	// __oled_H
