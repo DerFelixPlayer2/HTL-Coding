127b5d70904db86c9e18d293b7830844f8df882b
Felix <40723033+DerFelixPlayer2@users.noreply.github.com>
2022-11-04 14:20:32 +0100

    ADC_16

--- PlatformIO/src/main.cpp
+++ PlatformIO/src/main.cpp
@@ -1,6 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#include "../lib/OLED_SSD1306/oled_ssd1306.h"
+#include "../lib/ADC/ADC_16.hpp"
 
-int main() { OLED_SSD1306::main(); }
+int main() { ADC_16::main(); }
