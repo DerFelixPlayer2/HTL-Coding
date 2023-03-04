428893ef519732abe2ae23048134acca3e61aef5
Felix <40723033+DerFelixPlayer2@users.noreply.github.com>
2023-03-04 10:36:51 +0100

    QSORT Cleanup

--- 2023/Vorbereitung 1/Probecontest/QSORT/main.cpp
+++ 2023/Vorbereitung 1/Probecontest/QSORT/main.cpp
@@ -1,44 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <algorithm>
 
 using namespace std;
 
 vector<int> res;
 
-template<class Iterator>
-int quicksort(Iterator bgn, Iterator end) {
-	if (bgn == end)
-		return 0;
-	int p = *bgn;
-	auto mid = std::stable_partition(bgn, end, [p](int x) {
-		return x < p;
-	});
-	return 1 + std::max(quicksort(bgn, mid),
-						quicksort(mid + 1, end));
-}
-
-int s(vector<int> v) {
-	return quicksort(v.begin(), v.end());
-}
-
-void p(int i) {
-	//cout << "pushing " << i << endl;
-	res.push_back(i);
-}
-
 void recursive_fill_res(int lower_bound, int higher_bound) {
-	//cout << lower_bound << " " << higher_bound << endl;
-	if (lower_bound >= higher_bound) p(lower_bound);
+	if (lower_bound >= higher_bound) res.push_back(lower_bound);
 	else {
 		int mid = (higher_bound + lower_bound) / 2;
 		if (mid == lower_bound) {
-			p(lower_bound);
-			p(higher_bound);
+			res.push_back(lower_bound);
+			res.push_back(higher_bound);
 			return;
 		}
-		p(mid);
+		res.push_back(mid);
 		recursive_fill_res(lower_bound, mid - 1);
 		recursive_fill_res(mid + 1, higher_bound);
 	}
@@ -46,8 +23,6 @@ void recursive_fill_res(int lower_bound, int higher_bound) {
 
 int main() {
 	ios_base::sync_with_stdio(false);
-
-	// cout << "t " << s({2, 1, 4, 3}) << endl;
 
 	int n, k, i;
 	cin >> n >> k;
@@ -58,7 +33,7 @@ int main() {
 	}
 
 	res.reserve(n);
-	for (i = 1; i + log2(n - i) < k;) p(i++);
+	for (i = 1; i + log2(n - i) < k;) res.push_back(i++);
 
 	if (i - 1 < n) recursive_fill_res(i, n);
 
