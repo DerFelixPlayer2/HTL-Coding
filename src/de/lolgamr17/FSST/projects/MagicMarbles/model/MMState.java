a2e24645542e6906ac2d8548b8eab01399eeec47
Felix <40723033+DerFelixPlayer2@users.noreply.github.com>
2023-04-26 16:18:17 +0200

    MagicMarbles Part 1 (Console Application)

---
+++ src/de/lolgamr17/FSST/projects/MagicMarbles/model/MMState.java
@@ -0,0 +1,11 @@
+package de.lolgamr17.FSST.projects.MagicMarbles.model;
+
+/**
+ * The different possible states of a game.
+ */
+public enum MMState {
+	/** The game is still running, i.e. fields can be opened. */
+	RUNNING,
+	/** The game is terminated, i.e., there are no more two equally colored marbles next to each other. */
+	END
+}
