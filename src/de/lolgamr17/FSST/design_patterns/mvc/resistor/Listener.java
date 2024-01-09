3ad13ddfa53759fe98fe0ef1ab1bac1dfa6b4598
Felix <40723033+DerFelixPlayer2@users.noreply.github.com>
2024-01-09 15:47:20 +0100

    MVC Example (unfinished)

---
+++ src/de/lolgamr17/FSST/design_patterns/mvc/resistor/Listener.java
@@ -0,0 +1,13 @@
+package de.lolgamr17.FSST.design_patterns.mvc.resistor;
+
+import java.util.EventListener;
+
+public interface Listener extends EventListener {
+
+    void onVoltageChanged(VoltageChangedEvent e);
+
+    void onCurrentChanged(CurrentChangedEvent e);
+
+    void onModeChanged(ModeChangedEvent e);
+
+}
---
+++ src/de/lolgamr17/FSST/design_patterns/mvc/resistor/Mode.java
@@ -0,0 +1,6 @@
+package de.lolgamr17.FSST.design_patterns.mvc.resistor;
+
+public enum Mode {
+    VOLTAGE,
+    CURRENT
+}
