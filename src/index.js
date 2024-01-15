a0bcf6213d92d82555002256b4d0513ae9c0cba2
Felix <40723033+DerFelixPlayer2@users.noreply.github.com>
2024-01-15 15:56:33 +0100

    Refactoring

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import express from "express";
 import path from "path";
 import url from "url";
 
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const __assets_dir = path.join(__dirname, "..", "assets");
+
 const app = express();
 
-app.use(express.static(path.join(path.dirname(url.fileURLToPath(import.meta.url)))));
+app.use(express.static(__dirname));
+app.use(express.static(__assets_dir));
 
 app.get("/", (req, res) => {
     return res
         .header("Access-Control-Allow-Origin", "*")
         .status(200)
-        .sendFile(path.join(path.dirname(url.fileURLToPath(import.meta.url)), "index.html"));
+        .sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(3000, console.info);
